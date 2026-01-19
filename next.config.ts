d55f36baf1ed901302f47ea43cd24f3703fb125a
saran-srinivasan <117505965+saran-srinivasan@users.noreply.github.com>
2026-01-19 19:51:56 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/nambu-coir-prototype",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
